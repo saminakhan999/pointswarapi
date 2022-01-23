856fb7a3af88f8278702a788be0f32f92282756a
saminakhan999 <Samina.khan9999@gmail.com>
2022-01-23 20:49:38 +0000

    try heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('./app');
 const port = 3000;
 
-app.listen(port, () => { console.log(`Express now departing from port ${port}`)})
 
 
+const port = process.env.PORT || 3000; // if there is no PORT env variable, 3000 will be used
+app.listen(port, () => console.log(`Express is running on port ${port}`))
